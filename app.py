527afcd9d7ab5ca8db391854d62352f69c59da46
Jagathish <kingsmily25@gmail.com>
2026-06-20 19:29:09 +0530

    enable groq ai

--- app.py
+++ app.py
@@ -41,22 +41,41 @@ def get_ai_response(message):
 
     response = requests.post(url, headers=headers, json=data)
 
-    print(response.text)
+    print("STATUS:", response.status_code)
+    print("BODY:", response.text)
 
     result = response.json()
 
     if "choices" not in result:
-        return f"ERROR: {result}"
+        return f"Groq Error: {result}"
 
     return result["choices"][0]["message"]["content"]
-# ---------------- ROUTES ----------------
 @app.route("/")
 def home():
     return render_template("index.html")
 
 @app.route("/chat", methods=["POST"])
 def chat():
-    return jsonify({"reply": "Bot working successfully!"})
+    user_msg = request.json["message"]
 
+    reply = get_ai_response(user_msg)
+
+    conn = sqlite3.connect("chatbot.db")
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "INSERT INTO messages (role, content) VALUES (?, ?)",
+        ("user", user_msg)
+    )
+
+    cursor.execute(
+        "INSERT INTO messages (role, content) VALUES (?, ?)",
+        ("bot", reply)
+    )
+
+    conn.commit()
+    conn.close()
+
+    return jsonify({"reply": reply})
 if __name__ == "__main__":
     app.run(debug=True)
